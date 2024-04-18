24962ac44083378d2d1df080a3050c136b995b68
Ahmed <heydude08080@gmail.com>
2024-04-18 23:54:41 +0500

    backend complete

--- backend/todo_app/main.py
+++ backend/todo_app/main.py
@@ -1,8 +1,8 @@
-from fastapi import FastAPI
-from sqlmodel import SQLModel,Field,create_engine,Session
+from fastapi import FastAPI,Depends,HTTPException
+from sqlmodel import SQLModel,Field,create_engine,Session,select
 from todo_app import setting 
-from todo_app.setting import DATABASE_URL
-
+from typing import Annotated
+from contextlib import asynccontextmanager
 #Create Model
 class Todo (SQLModel, table=True):
     id:int | None = Field(default=None, primary_key=True)
@@ -13,30 +13,89 @@ connection_string:str = str(setting.DATABASE_URL).replace("postgresql","postgres
 
 engine = create_engine(connection_string,connect_args={"sslmode":"require"},pool_recycle=300, echo=True)
 
-SQLModel.metadata.create_all(engine)
+def create_tables():
+ SQLModel.metadata.create_all(engine)
 
-todo1 : Todo = Todo(content="First todo")
-todo2 : Todo = Todo(content="Second todo")
+# todo1 : Todo = Todo(content="First todo")
+# todo2 : Todo = Todo(content="Second todo")
 
-session = Session(engine)
+# session = Session(engine)
 
-#Session
-session.add(todo1)
-session.add(todo2)
-print(f"Before commit {todo1}")
-session.commit()
-print(f"After commit {todo1}")
-session.close()
+# #Session
+# session.add(todo1)
+# session.add(todo2)
+# print(f"Before commit {todo1}")
+# session.commit()
+# print(f"After commit {todo1}")
+# session.close()
 
 
-app = FastAPI()
+def get_session():
+    with Session(engine) as session:
+        yield session
 
-# Define root endpoint
+
+@asynccontextmanager
+async def lifespan(app:FastAPI):
+    print("creating tables")
+    create_tables()
+    print("tables created")
+    yield
+
+
+
+app = FastAPI(lifespan=lifespan, title="TODO APP", version="2.1.0")
+
 @app.get("/")
 async def root():
-    return {"message": "Hello"}
+    return {"Hello"}
 
-# Run FastAPI server with uvicorn
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+@app.post('/todos/',response_model=Todo)
+async def create_todo(todo:Todo,session:Annotated[Session,Depends(get_session)]):
+    session.add(todo)
+    session.commit()
+    session.refresh(todo)
+    return todo
+
+@app.get("/todos/", response_model=list[Todo])
+async def get_all(session:Annotated[Session,Depends(get_session)]):
+    todos = session.exec(select(Todo)).all()
+    if todos:
+     return todos
+    else:
+        HTTPException(status_code=404, detail="No task found")
+
+@app.get("/todos/{id}",response_model=Todo)
+async def get_single_todo(id:int, session:Annotated[Session,Depends(get_session)]):
+
+    todo = session.exec(select(Todo).where(Todo.id==id)).first()
+    if todo:
+     return todo
+    else:
+        raise HTTPException(status_code=404, detail="No task found") 
+    
+    
+@app.put("/todos/{id}")
+async def edit_todo(id:int,todo:Todo, session:Annotated[Session,Depends(get_session)]):
+     existing_todo = session.exec(select(Todo).where(Todo.id==id)).first()
+     if existing_todo:
+         existing_todo.content = todo.content
+         existing_todo.is_completed = todo.is_completed
+         session.add(existing_todo)
+         session.commit()
+         session.refresh(existing_todo)
+         return existing_todo
+     else:
+         raise HTTPException(status_code=404, detail="No task found")
+
+@app.delete("/todo/{id}")
+async def del_todo(id:int, session:Annotated[Session,Depends(get_session)]):
+    # todo = session.exec(select(Todo).where(Todo.id==id)).first()
+    todo = session.get(Todo,id)
+    if todo:
+        session.delete(todo)
+        session.commit()
+      
+        return {"message":"Task successfully deleted"}
+    else:
+        raise HTTPException(status_code=404, detail="No task found")
--- backend/todo_app/setting.py
+++ backend/todo_app/setting.py
@@ -1,8 +1,11 @@
 from starlette.config import Config
 from starlette.datastructures import Secret
 
-# Load configuration from .env file
-config = Config(".env")
 
-# Retrieve DATABASE_URL from environment variables
-DATABASE_URL = config("DATABASE_URL", cast=Secret)
+try:
+    config = Config(".env")
+except FileNotFoundError:
+ config = Config()
+
+
+DATABASE_URL = config("DATABASE_URL",cast=Secret)
