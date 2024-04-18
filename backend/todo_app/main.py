d75f86a3c76390b48944b00248fded9d0873b727
Ahmed <heydude08080@gmail.com>
2024-04-18 20:23:31 +0500

    create sql model

--- backend/todo_app/main.py
+++ backend/todo_app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
-app = FastAPI()
+from sqlmodel import SQLModel,Field 
 
-@app.get("/")
-async def todo():
-    return {"Message":"hola"}
+
+
+class Todo (SQLModel, table=True):
+    id:int | None = Field(default=None, primary_key=True)
+    content:str = Field(index=True,min_length=3,max_length=54)
+    is_completed : bool = Field(default=False)
