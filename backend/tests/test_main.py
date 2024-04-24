4d0c21c599e5ab100df7feb50624009db78a2a88
Ahmed <heydude08080@gmail.com>
2024-04-25 03:21:19 +0500

    write post test with pytest

--- backend/tests/test_main.py
+++ backend/tests/test_main.py
@@ -1,8 +1,8 @@
 from fastapi.testclient import TestClient
 from fastapi import FastAPI
-from sqlmodel import create_engine
+from sqlmodel import create_engine,SQLModel,Session
 from todo_app import setting
-from todo_app.main import app
+from todo_app.main import app,get_session
 
 
 connection_string:str = str(setting.TEST_DATABASE_URL).replace("postgresql","postgresql+psycopg")
@@ -17,3 +17,16 @@ def test_root():
     assert response.status_code == 200
     assert data == {"message":"Welcome to ToDo App"}
     
+#TEST 2: post test
+def test_create_todo():
+    SQLModel.metadata.create_all(engine)
+    with Session(engine) as session:
+        def db_session_override():
+            return session
+    app.dependency_overrides[get_session] = db_session_override 
+    client = TestClient(app=app)
+    test_todo = {"content":"create todo test", "is_completed":False}
+    response = client.post("/todos",json=test_todo) 
+    data = response.json()
+    assert response.status_code == 200
+    assert data["content"] == test_todo["content"]
