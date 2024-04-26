d12e3d978a4a8a55bb8c611f3fa81b4d926dc76e
Ahmed <heydude08080@gmail.com>
2024-04-26 18:39:01 +0500

    write some tests using pytest

--- backend/tests/test_main.py
+++ backend/tests/test_main.py
@@ -30,3 +30,38 @@ def test_create_todo():
     data = response.json()
     assert response.status_code == 200
     assert data["content"] == test_todo["content"]
+
+#TEST 3: get all
+
+def test_get_all():
+    SQLModel.metadata.create_all(engine)
+    with Session(engine) as session:
+        def db_session_override():
+            return session
+    app.dependency_overrides[get_session] == db_session_override
+    client = TestClient(app=app)
+    test_todo = {"content":"get all todos test","is_completed":False}
+    response = client.post("/todos/",json=test_todo)
+    data = response.json()
+    response = client.get("/todos")
+    new_todo = response.json()[-1]
+    assert response.status_code == 200
+    assert new_todo["content"] == test_todo["content"]
+
+
+#TEST 4: single todo
+def test_get_single_todo():
+    SQLModel.metadata.create_all(engine)
+    with Session(engine) as session:
+        def db_session_ovverride():
+         return session
+         
+    app.dependency_overrides[get_session] == db_session_ovverride
+    client = TestClient(app=app)
+    test_todo = {"content":"get single todo","is_complete":False}
+    response = client.post("todos/",json=test_todo)
+    todo_id = response.json()["id"]
+    res = client.get(f"/todos/{todo_id}")
+    data = res.json()
+    assert res.status_code == 200
+    assert data["content"] == test_todo["content"]
